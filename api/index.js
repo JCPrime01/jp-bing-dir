b2fffaa95c254c8c2ac06fe9e06918406edd075b
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 10:31:36 -0300

    update shareCode W2OIEFMSGW7

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HT1XFBUQG7Q&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=W2OIEFMSGW7&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
