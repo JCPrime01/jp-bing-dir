8787712a8333b95bd33c799e059e9fb6882bda1d
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 09:45:26 -0300

    chore: shareCode BQHOQHGIHI9 -> KACFW31AUJK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=BQHOQHGIHI9&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=KACFW31AUJK&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
