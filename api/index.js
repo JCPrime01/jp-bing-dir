94d41968ee400977912ef1cd3a9a2944a7fb0217
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 09:24:14 -0300

    chore: shareCode AYXDB5T2SYW -> 2A08RU0LIZL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=AYXDB5T2SYW&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2A08RU0LIZL&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
