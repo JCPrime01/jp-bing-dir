37fadc6c645f1101cd78e5bd741b694cbe6b9567
JCPrime01 <vanzokiller@gmail.com>
2026-06-12 09:15:52 -0300

    chore: shareCode 3SMUC32X7E3 -> Z5VP7J7ULH6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=3SMUC32X7E3&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=Z5VP7J7ULH6&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
