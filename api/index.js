3302fad5e2a23de79905eb8c6297f5ba7495156d
JCPrime01 <vanzokiller@gmail.com>
2026-06-16 09:24:59 -0300

    chore: shareCode G8U9KQ1QH60 -> 294693650-38 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=G8U9KQ1QH60&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=294693650-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
