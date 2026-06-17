2ff495287e594f45626a37ebaacfb1150c4f0f91
JCPrime01 <vanzokiller@gmail.com>
2026-06-17 10:23:27 -0300

    chore: shareCode 6GK1NRYQH33 -> NTUVN62EPPT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6GK1NRYQH33-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=NTUVN62EPPT-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
