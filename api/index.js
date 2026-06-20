e6d1b2ee613ecfd1dca882411d57252588ae7f53
JCPrime01 <vanzokiller@gmail.com>
2026-06-20 10:55:59 -0300

    chore: shareCode Z3URF9S3IM4 -> EJSGF6LU4Z2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=Z3URF9S3IM4-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=EJSGF6LU4Z2-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
