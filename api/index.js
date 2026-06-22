686057da39a161a35ffcb1ae5bf1ba1a57904a96
JCPrime01 <vanzokiller@gmail.com>
2026-06-22 09:58:03 -0300

    chore: shareCode NW9HM5NBZ9Y -> ON51UVRLQVP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=NW9HM5NBZ9Y-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ON51UVRLQVP-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
