bb3ffd76030480b586061856d785ddf8ab2c40bc
JCPrime01 <vanzokiller@gmail.com>
2026-06-22 09:58:52 -0300

    chore: shareCode ON51UVRLQVP -> LQIY00KZ7NY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ON51UVRLQVP-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=LQIY00KZ7NY-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
