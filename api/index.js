b8d1d6cf68ad19d44450210b533becdcb0eaf209
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 11:56:51 -0300

    chore: shareCode WTQYJFRG5YI -> BSRWI29C6QW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=WTQYJFRG5YI-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=BSRWI29C6QW-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
