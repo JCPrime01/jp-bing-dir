cace3d23b57e149cc7fbeaac220673bd2f5653d9
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 11:56:47 -0300

    chore: shareCode 6W3K3PBE42A -> XRDPDISNEO5 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6W3K3PBE42A-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=XRDPDISNEO5-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
