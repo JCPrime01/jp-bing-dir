d9a203eb9761b8b4813aae917641c511b7be9546
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 09:20:30 -0300

    chore: shareCode F22U59TO5ZD -> QHCW6VJQNLR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=F22U59TO5ZD-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=QHCW6VJQNLR-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
