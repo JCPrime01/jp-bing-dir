68ddb73057d39cec03b32f9f7da63ef18720989c
JCPrime01 <vanzokiller@gmail.com>
2026-07-21 12:43:03 -0300

    chore: shareCode K4ZDOIZ3LJ2 -> LANJ55EN3EN (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=K4ZDOIZ3LJ2-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=LANJ55EN3EN-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
