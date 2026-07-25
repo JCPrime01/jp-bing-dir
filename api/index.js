a98cd44d8b95dbb388d7690e172799310ef2dfda
JCPrime01 <vanzokiller@gmail.com>
2026-07-25 12:23:32 -0300

    chore: shareCode NA60UDLMFK6 -> ASWWVALDVW1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=NA60UDLMFK6-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ASWWVALDVW1-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
