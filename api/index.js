d317509fc1f766523e62acc1e59fa3f1cfd02f72
JCPrime01 <vanzokiller@gmail.com>
2026-07-25 12:30:15 -0300

    chore: shareCode ASWWVALDVW1 -> 02ICSGAXQOU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ASWWVALDVW1-38&afp2=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=02ICSGAXQOU-38&afp2=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
